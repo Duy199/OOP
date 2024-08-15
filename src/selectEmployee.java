515e397c53656b0a4b2c25e1a53a49dcc0f6b399
Admin <thanhduy.tran3008@gmail.com>
2024-08-15 12:51:00 +0700

    Build my Gui for salary management - ongoing

--- src/selectEmployee.java
+++ src/selectEmployee.java
@@ -120,65 +120,93 @@ public class selectEmployee extends JFrame {
                                 if (!maSo.getText ().matches ("-?\\d+")) {
                                     JOptionPane.showMessageDialog (selectEmployee.this,"ma so must be numbering");
                                 }
-                                else if (hoTen.getText ().matches ("-?\\d+")) {
-                                    JOptionPane.showMessageDialog (selectEmployee.this,"Ho ten must be wording");
+                                else if (!hoTen.getText ().matches ("[a-zA-Z\\s]+")) {
+                                    JOptionPane.showMessageDialog (selectEmployee.this,"Ho ten must be wording and spacing");
                                 }
-                                else if (!namSinh.getText ().matches ("-?\\d+")) {
-                                    JOptionPane.showMessageDialog (selectEmployee.this,"Nam sinh must be numbering");
+                                else if (!namSinh.getText ().matches ("\\d{4}")) {
+                                    JOptionPane.showMessageDialog (selectEmployee.this,"Nam sinh must be numbering with 4 digits");
                                 }
                                 else {
                                     GVCoHuu gvCoHuu = new GVCoHuu (maSo.getText (),hoTen.getText (),Integer.parseInt (namSinh.getText ()),Double.parseDouble (textField1.getText ()),Double.parseDouble (textField2.getText ()),Double.parseDouble (textField3.getText ()));
                                     textField4.setText (Double.toString (gvCoHuu.tinhLuong ()));
                                     saveDataButton.setVisible (true);
-                                    //Update to database
-                                    saveDataButton.addActionListener (new ActionListener ( ) {
-                                        @Override
-                                        public void actionPerformed(ActionEvent e) {
 
-
-                                            try {
-                                                //Connect to database
-                                                Connection connection = DriverManager.getConnection ("jdbc:postgresql://localhost:3008/NewDB","postgres","Liverpool3008@");
-
-                                                //Check similar maSo, hoTen
-                                                PreparedStatement preparedStatement = connection.prepareStatement ("SELECT * FROM GVCoHuu WHERE maso = ? OR hoten = ?");
-                                                preparedStatement.setString (1,maSo.getText ());
-                                                preparedStatement.setString (2,hoTen.getText ());
-                                                ResultSet resultSet = preparedStatement.executeQuery();
-                                                if (resultSet.next ()) {
-
-                                                    if (maSo.getText ().equals (String.valueOf (resultSet.getInt (1)))) {
-                                                        JOptionPane.showMessageDialog (selectEmployee.this,"This ID already existed in our Database");
-                                                    }
-                                                    if (hoTen.getText ().equals (resultSet.getString (2))) {
-                                                        JOptionPane.showMessageDialog (selectEmployee.this,"This full name already existed in our Database");
-                                                    }
-                                                }
-                                                //Update
-                                                else {
-                                                    JOptionPane.showMessageDialog (selectEmployee.this,"Updated to database");
-                                                    String insertSQL = "INSERT INTO GVCoHuu (maso, hoten, namsinh, hsl, lcs, hsthamnien, luong) VALUES (?, ?, ?, ?, ?, ?, ?)";
-                                                    PreparedStatement PreparedStatement = connection.prepareStatement(insertSQL);
-                                                    PreparedStatement.setInt (1, Integer.parseInt (maSo.getText ()));
-                                                    PreparedStatement.setString(2, hoTen.getText ());
-                                                    PreparedStatement.setInt (3, Integer.parseInt (namSinh.getText ()));
-                                                    PreparedStatement.setDouble (4, Double.parseDouble (textField1.getText ()));
-                                                    PreparedStatement.setDouble (5, Double.parseDouble (textField2.getText ()));
-                                                    PreparedStatement.setDouble (6, Double.parseDouble (textField3.getText ()));
-                                                    PreparedStatement.setDouble (7, Double.parseDouble (textField4.getText ()));
-                                                    PreparedStatement.executeUpdate();
-                                                }
-                                            }
-                                            catch (SQLException E) {
-                                                throw new RuntimeException (E);
-                                            }
-                                        }
-                                    });
                                 }
                             }
                         }
                     });
+                    saveDataButton.addActionListener (new ActionListener ( ) {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
 
+                            try {
+                                //Connect to database
+                                Connection connection = DriverManager.getConnection ("jdbc:postgresql://localhost:3008/NewDB","postgres","Liverpool3008@");
+
+                                //Check similar maSo, hoTen
+                                PreparedStatement preparedStatement = connection.prepareStatement ("SELECT * FROM GVCoHuu WHERE maso = ? OR hoten = ?");
+                                preparedStatement.setString (1,maSo.getText ());
+                                preparedStatement.setString (2,hoTen.getText ());
+                                ResultSet resultSet = preparedStatement.executeQuery();
+                                if (resultSet.next ()) {
+
+                                    if (maSo.getText ().equals (String.valueOf (resultSet.getInt (1)))) {
+                                        JOptionPane.showMessageDialog (selectEmployee.this,"This ID already existed in our Database");
+
+                                    }
+                                    if (hoTen.getText ().equals (resultSet.getString (2))) {
+                                        JOptionPane.showMessageDialog (selectEmployee.this,"This full name already existed in our Database");
+
+                                    }
+                                }
+                                //Update
+                                else {
+                                    JOptionPane.showMessageDialog (selectEmployee.this,"Updated to database");
+                                    String insertSQL = "INSERT INTO GVCoHuu (maso, hoten, namsinh, hsl, lcs, hsthamnien, luong) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                                    PreparedStatement PreparedStatement = connection.prepareStatement(insertSQL);
+                                    PreparedStatement.setInt (1, Integer.parseInt (maSo.getText ()));
+                                    PreparedStatement.setString(2, hoTen.getText ());
+                                    PreparedStatement.setInt (3, Integer.parseInt (namSinh.getText ()));
+                                    PreparedStatement.setDouble (4, Double.parseDouble (textField1.getText ()));
+                                    PreparedStatement.setDouble (5, Double.parseDouble (textField2.getText ()));
+                                    PreparedStatement.setDouble (6, Double.parseDouble (textField3.getText ()));
+                                    PreparedStatement.setDouble (7, Double.parseDouble (textField4.getText ()));
+                                    PreparedStatement.executeUpdate();
+                                    //reset to default page
+                                    comboBox1.setSelectedIndex (0);
+                                    namSinhLabel.setVisible (false);
+                                    maSoLabel.setVisible (false);
+                                    hoTenLabel.setVisible (false);
+                                    label1.setVisible (false);
+                                    label2.setVisible (false);
+                                    label3.setVisible (false);
+                                    label4.setVisible (false);
+                                    label5.setVisible (false);
+                                    maSo.setVisible (false);
+                                    hoTen.setVisible (false);
+                                    namSinh.setVisible (false);
+                                    textField1.setVisible (false);
+                                    textField2.setVisible (false);
+                                    textField3.setVisible (false);
+                                    textField4.setVisible (false);
+                                    textField5.setVisible (false);
+                                    saveDataButton.setVisible (false);
+                                    clickToCalculateButton.setVisible (false);
+                                    //reset all value to zero
+                                    maSo.setText (null);
+                                    hoTen.setText (null);
+                                    namSinh.setText (null);
+                                    textField1.setText (null);
+                                    textField2.setText (null);
+                                    textField3.setText (null);
+                                    textField4.setText (null);
+                                }
+                            }
+                            catch (SQLException E) {
+                                throw new RuntimeException (E);
+                            }
+                        }
+                    });
                 }
                 else if (comboBox1.getSelectedItem ().equals ("2. Nhan vien co huu")) {
                     //write code
